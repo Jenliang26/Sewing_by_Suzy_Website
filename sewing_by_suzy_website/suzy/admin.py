98c77fbc1df6f3f213775fc52422a4efdbdfc6ac
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-14 21:52:41 -0500

    Registered all my models

--- sewing_by_suzy_website/suzy/admin.py
+++ sewing_by_suzy_website/suzy/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User, Customer, Orders, Statuses, Garment, Inventory, Reviews
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Orders)
+admin.site.register(Statuses)
+admin.site.register(Garment)
+admin.site.register(Inventory)
+admin.site.register(Reviews)
+
