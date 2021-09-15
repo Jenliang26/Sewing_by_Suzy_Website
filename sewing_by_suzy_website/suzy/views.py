18e20f83875150aa467824b420e81428cfc4ef74
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-15 16:21:11 -0500

    Created User, Employee, and customer class in views to make API requests

--- sewing_by_suzy_website/suzy/views.py
+++ sewing_by_suzy_website/suzy/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from .models import User, Customer, Orders, Statuses, Garment, Inventory, Reviews
+from .serializers import UserSerializer, CustomerSerializer, OrdersSerializer, StatusesSerializer, GarmentSerializer, InventorySerializer, ReviewsSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
 # Create your views here.
+
+class CreateUser(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CreateCustomer(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CreateEmployee(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DisplayCustomer(APIView):
+
+    def get_customer(self, pk):
+        try:
+            return Customer.objects.get(pk=pk)
+        except Customer.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        customer = self.get_customer(pk)
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data)
+
+        
