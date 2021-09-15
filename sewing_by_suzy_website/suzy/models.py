5bfc6f2047890136a13ae7fd2f61c50b4ec41d17
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-14 21:25:31 -0500

    Updated number_rating field in Reviews to include rating restrictions

--- sewing_by_suzy_website/suzy/models.py
+++ sewing_by_suzy_website/suzy/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.expressions import Value
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -13,6 +14,20 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=12)
     email = models.EmailField(max_length=50)
 
+class Orders(models.Model):
+    customer = models.ForeignKey('Customer', blank=True, null=True, on_delete=models.PROTECT)
+    date = models.DateField(null=True)
+    notes = models.CharField(max_length=500)
+    status = models.ForeignKey('Statuses', blank=True, null=True, on_delete=models.PROTECT)
+
+class Statuses(models.Model):
+    status = models.CharField(max_length=80)
+
+class Garment(models.Model):
+    order = models.ForeignKey('Order', blank=True, null=True, on_delete=models.PROTECT)
+    type = models.CharField(max_length=50)
+    quantity = models.IntegerField(null = True)
+
 class Inventory(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
@@ -22,17 +37,6 @@ class Inventory(models.Model):
 class Reviews(models.Model):
     name = models.CharField(max_length=50)
     date = models.DateField(null=True)
-    number_rating = models.IntegerField('')
+    number_rating = models.IntegerField(default= 4, validators=[MaxValueValidator(5), MinValueValidator(1)])
     comment = models.CharField(max_length=500)
-
-class Orders(models.Model):
-    customer = models.ForeignKey('Customer', blank=True, null=True, on_delete=models.PROTECT)
-    date = models.DateField(null=True)
-    notes = models.CharField(max_length=500)
-    status = models.IntegerField('') #make it like the rating? 1,2,3?
-
-class Garment(models.Model):
-    order = models.ForeignKey('Order', blank=True, null=True, on_delete=models.PROTECT)
-    type = models.CharField(max_length=50)
-    quantity = models.IntegerField(null = True)
 
