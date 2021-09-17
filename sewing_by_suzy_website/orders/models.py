3f056397787a1ae196400cab713d4a3931714144
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-17 00:53:36 -0500

    Created Order class in Order app

--- sewing_by_suzy_website/orders/models.py
+++ sewing_by_suzy_website/orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from accounts.models import Customer
 
 # Create your models here.
+class Orders(models.Model):
+    customer = models.ForeignKey('Customer', blank=True, null=True, on_delete=models.PROTECT)
+    date = models.DateField(null=True)
+    notes = models.CharField(max_length=500)
+    status = models.ForeignKey('Statuses', blank=True, null=True, on_delete=models.PROTECT)
