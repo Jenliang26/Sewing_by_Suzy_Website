30a46040fc1aa9930f3f76b4d6b1f598c96b4445
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-17 00:58:26 -0500

    Created Reviews class in the Reviews app

--- sewing_by_suzy_website/reviews/models.py
+++ sewing_by_suzy_website/reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Reviews(models.Model):
+    name = models.CharField(max_length=50)
+    date = models.DateField(null=True)
+    number_rating = models.IntegerField(default= 4, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    comment = models.CharField(max_length=500)
--- sewing_by_suzy_website/suzy/models.py
+++ sewing_by_suzy_website/suzy/models.py
@@ -1,33 +1,11 @@
-from django.db import models
-from django.db.models.expressions import Value
-from django.core.validators import MaxValueValidator, MinValueValidator
-from django.contrib.auth.models import User
+#from django.db import models
+#from django.db.models.expressions import Value
+#from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Orders(models.Model):
-    customer = models.ForeignKey('Customer', blank=True, null=True, on_delete=models.PROTECT)
-    date = models.DateField(null=True)
-    notes = models.CharField(max_length=500)
-    status = models.ForeignKey('Statuses', blank=True, null=True, on_delete=models.PROTECT)
 
-class Statuses(models.Model):
-    status = models.CharField(max_length=80)
 
-class Garment(models.Model):
-    order = models.ForeignKey('Orders', blank=True, null=True, on_delete=models.PROTECT)
-    type = models.CharField(max_length=50)
-    quantity = models.IntegerField(null = True)
 
-class Inventory(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
-    quantity = models.IntegerField(null = True)
-    category = models.CharField(max_length=50)
 
-class Reviews(models.Model):
-    name = models.CharField(max_length=50)
-    date = models.DateField(null=True)
-    number_rating = models.IntegerField(default= 4, validators=[MaxValueValidator(5), MinValueValidator(1)])
-    comment = models.CharField(max_length=500)
 
