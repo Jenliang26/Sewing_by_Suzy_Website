a17f6b5d7e86a58a2c865ec91d461112c19e570f
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-17 13:32:06 -0500

    Fixed lazy reference in orders app to reference FK

--- sewing_by_suzy_website/suzy/serializers.py
+++ sewing_by_suzy_website/suzy/serializers.py
@@ -3,8 +3,6 @@ from django.db.models import fields
 from rest_framework import serializers
 from .models import User, Customer, Orders, Statuses, Garment, Inventory, Reviews
 
-class UserSerializer(serializers.ModelSerializer):
-
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
