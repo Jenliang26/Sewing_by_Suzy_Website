7bb050fc06d9ab8f7044fe0696c134feb01df918
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-17 15:23:49 -0500

    Completed inventory serializer

--- sewing_by_suzy_website/inventory/views.py
+++ sewing_by_suzy_website/inventory/views.py
@@ -1,3 +1,55 @@
+from django.http.response import Http404
 from django.shortcuts import render
+from rest_framework import serializers, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Inventory
+from .serializers import InventorySerializer
+from django.contrib.auth.models import User
+from django.http import Http404
+
 
 # Create your views here.
+class InventoryList (APIView):
+
+    def get(self, request):
+        inventory = Inventory.objects.all()
+        serializer = InventorySerializer(inventory, many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = InventorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class InventoryDetail(APIView):
+
+    def get_inventory(self, pk):
+        try:
+            return Inventory.objects.get(pk=pk)
+        except Inventory.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        inventory = self.get_inventory(pk)
+        serializer = InventorySerializer(inventory)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        inventory = self.get_inventory(pk)
+        serializer = InventorySerializer(inventory, data=request.data)
+        if serializer.is_valid():
+            serializer.update(inventory, request.data)
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        inventory = self.get_inventory(pk)
+        serializer = InventorySerializer(inventory)
+        inventory.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
