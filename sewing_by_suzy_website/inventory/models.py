80b1e617ba3d53776c2bff6749c1daced4b7ec9f
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-17 00:55:27 -0500

    Created inventory class in the inventory app

--- sewing_by_suzy_website/inventory/models.py
+++ sewing_by_suzy_website/inventory/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Inventory(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    quantity = models.IntegerField(null = True)
+    category = models.CharField(max_length=50)
