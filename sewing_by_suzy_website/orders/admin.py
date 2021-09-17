73d55cc4221d971f1884487887a4db02a8f68257
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-17 01:08:27 -0500

    Registered Orders, Statuses, and Garment in the Orders app

--- sewing_by_suzy_website/orders/admin.py
+++ sewing_by_suzy_website/orders/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Orders
+from .models import Orders, Statuses, Garment
 
 # Register your models here.
 admin.site.register(Orders)
+admin.site.register(Statuses)
+admin.site.register(Garment)
+
+
