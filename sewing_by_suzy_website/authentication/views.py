d9adf0e86f13724333ede0a751ad32903b96b2af
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-16 22:19:41 -0500

    Created register view class for authentication app

--- sewing_by_suzy_website/authentication/views.py
+++ sewing_by_suzy_website/authentication/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from django.contrib.auth.model import User
+from .serializers import RegistrationSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegistrationSerializer
+    
