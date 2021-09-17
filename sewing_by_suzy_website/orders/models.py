95050fd9ae895b216917048c5f53bf27c3b6d263
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-17 00:54:47 -0500

    Added Statuses and Garment class to the Order app

--- sewing_by_suzy_website/orders/models.py
+++ sewing_by_suzy_website/orders/models.py
@@ -7,3 +7,11 @@ class Orders(models.Model):
     date = models.DateField(null=True)
     notes = models.CharField(max_length=500)
     status = models.ForeignKey('Statuses', blank=True, null=True, on_delete=models.PROTECT)
+
+class Statuses(models.Model):
+    status = models.CharField(max_length=80)
+
+class Garment(models.Model):
+    order = models.ForeignKey('Orders', blank=True, null=True, on_delete=models.PROTECT)
+    type = models.CharField(max_length=50)
+    quantity = models.IntegerField(null = True)
