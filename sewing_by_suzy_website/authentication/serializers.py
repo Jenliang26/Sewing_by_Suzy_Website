62ae0be3ea22f91fa65219c84e11725326061a80
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-10-05 17:42:22 -0500

    Added id field to userserializer to return id when searching users

--- sewing_by_suzy_website/authentication/serializers.py
+++ sewing_by_suzy_website/authentication/serializers.py
@@ -31,7 +31,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User
-        fields = ['email', 'first_name', 'last_name'] 
+        fields = ['id', 'email', 'first_name', 'last_name'] 
 
 
 
