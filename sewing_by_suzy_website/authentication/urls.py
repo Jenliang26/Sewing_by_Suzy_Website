810f78d379abc73153b15b0cf8cfc371f6db73c2
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-10-05 17:42:45 -0500

    added a path to look up user profile and return all information

--- sewing_by_suzy_website/authentication/urls.py
+++ sewing_by_suzy_website/authentication/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, UserProfile
+from .views import RegisterView, UserProfile, AllProfiles
 from authentication import views
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='register'),
-    path('profile/<int:uid>/', views.UserProfile.as_view())
+    path('profile/<int:uid>/', views.UserProfile.as_view()),
+    path('profile/', views.AllProfiles.as_view())
 ]
--- sewing_by_suzy_website/authentication/views.py
+++ sewing_by_suzy_website/authentication/views.py
@@ -36,3 +36,10 @@ class UserProfile(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AllProfiles(APIView):
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
